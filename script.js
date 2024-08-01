bc5c1cabad54799251c95fd3b915ad6ffb1c69a5
ishitaa18 <arora.ishita2015@gmail.com>
2024-08-02 01:11:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,152 @@
+
+var loader = document.querySelector("#loaderr")
+setTimeout(function () {
+    loader.style.top = "-120%"
+}, 3700)
+var loader1 = document.querySelector("#loader1")
+setTimeout(function () {
+    loader1.style.top = "-120%"
+}, 3900)
+
+// gsap.from(" .aa ", {
+//     scrollTrigger: {
+//         trigger: ".aa",
+//         start: "top 80%",  // When the top of .abtme enters 80% of the viewport height
+//         end: "bottom 20%", // When the bottom of .abtme enters 20% of the viewport height
+//         toggleActions: "play none none none"
+//     },
+//     duration: 1.5,
+//     opacity: 0,
+//     y: 20,
+//     stagger: 0.2,
+//     ease: "power3.out"
+// });
+
+// gsap.from(" .bb ", {
+//     scrollTrigger: {
+//         trigger: ".bb",
+//         start: "top 80%",  // When the top of .abtme enters 80% of the viewport height
+//         end: "bottom 20%", // When the bottom of .abtme enters 20% of the viewport height
+//         toggleActions: "play none none none"
+//     },
+//     duration: 1.5,
+//     opacity: 0,
+//     delay:0.4,
+//     y: 20,
+//     stagger: 0.2,
+//     ease: "power3.out"
+// });
+
+// gsap.from(" .cc ", {
+//     scrollTrigger: {
+//         trigger: ".cc",
+//         start: "top 80%",  // When the top of .abtme enters 80% of the viewport height
+//         end: "bottom 20%", // When the bottom of .abtme enters 20% of the viewport height
+//         toggleActions: "play none none none"
+//     },
+//     duration: 1.5,
+//     opacity: 0,
+//     y: 20,
+//     delay:0.8,
+//     stagger: 0.2,
+//     ease: "power3.out"
+// });
+// gsap.from(" .cc ", {
+//     scrollTrigger: {
+//         trigger: ".cc",
+//         start: "top 80%",  // When the top of .abtme enters 80% of the viewport height
+//         end: "bottom 20%", // When the bottom of .abtme enters 20% of the viewport height
+//         toggleActions: "play none none none"
+//     },
+//     duration: 1.5,
+//     opacity: 0,
+//     x: 20,
+//     delay:0.8,
+//     stagger: 0.2,
+//     ease: "power3.out"
+// });
+
+gsap.from(" .heading1 ", {
+    scrollTrigger: {
+        trigger: ".heading1",
+        start: "top 80%",  // When the top of .abtme enters 80% of the viewport height
+        end: "bottom 20%", // When the bottom of .abtme enters 20% of the viewport height
+        toggleActions: "play none none none"
+    },
+    duration: 1.5,
+    opacity: 0,
+    delay:4,
+    y: 50,
+    stagger: 0.2,
+    ease: "power3.out"
+});
+
+gsap.from(" .heading2 ", {
+    scrollTrigger: {
+        trigger: ".heading2",
+        start: "top 80%",  // When the top of .abtme enters 80% of the viewport height
+        end: "bottom 20%", // When the bottom of .abtme enters 20% of the viewport height
+        toggleActions: "play none none none"
+    },
+    duration: 1.5,
+    opacity: 0,
+    y: 50,
+    stagger: 0.2,
+    ease: "power3.out"
+});
+
+gsap.from(" .heading3 ", {
+    scrollTrigger: {
+        trigger: ".heading3",
+        start: "top 80%",  // When the top of .abtme enters 80% of the viewport height
+        end: "bottom 20%", // When the bottom of .abtme enters 20% of the viewport height
+        toggleActions: "play none none none"
+    },
+    duration: 1.5,
+    opacity: 0,
+    y: 50,
+    stagger: 0.2,
+    ease: "power3.out"
+});
+
+gsap.from(" .heading4 ", {
+    scrollTrigger: {
+        trigger: ".heading4",
+        start: "top 80%",  // When the top of .abtme enters 80% of the viewport height
+        end: "bottom 20%", // When the bottom of .abtme enters 20% of the viewport height
+        toggleActions: "play none none none"
+    },
+    duration: 1.5,
+    opacity: 0,
+    y: 50,
+    stagger: 0.2,
+    ease: "power3.out"
+});
+
+gsap.from(" .heading5 ", {
+    scrollTrigger: {
+        trigger: ".heading5",
+        start: "top 80%",  // When the top of .abtme enters 80% of the viewport height
+        end: "bottom 20%", // When the bottom of .abtme enters 20% of the viewport height
+        toggleActions: "play none none none"
+    },
+    duration: 1.5,
+    opacity: 0,
+    y: 50,
+    stagger: 0.2,
+    ease: "power3.out"
+});
+
+gsap.from(" .heading6 ", {
+    scrollTrigger: {
+        trigger: ".heading6",
+        start: "top 80%",  // When the top of .abtme enters 80% of the viewport height
+        end: "bottom 20%", // When the bottom of .abtme enters 20% of the viewport height
+        toggleActions: "play none none none"
+    },
+    duration: 1.5,
+    opacity: 0,
+    y: 50,
+    stagger: 0.2,
+    ease: "power3.out"
+});
